3fa2c1210fb0dabeec56ac2c2b2f5766bbcd1306
DanielWindChoi <goyeon91@gmail.com>
2021-12-29 13:18:27 +0900

    my_to_do_list

---
+++ ToDoList/my_to_do_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+# view와 연결
+from . import views
+
+urlpatterns = [
+  # 해당 url 패턴을 views.py의 index 함수와 연결
+  path('', views.index, name = 'index'),
+  
+  # createTodo에 대한 URL 요청과 view를 연결해줍니다. 
+  path('createTodo/', views.createTodo),
+]
